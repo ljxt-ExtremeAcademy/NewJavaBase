3fc10c1d6d125a4759643414e954e60195d1eff3
窖头 <@DESKTOP-PJ6BUH1>
2017-07-06 20:46:09 +0800

    今儿真高兴，建立了第一个git项目，提交看看效果！

---
+++ src/com/xuetang9/kenny/javabase/chapter1/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.xuetang9.kenny.javabase.chapter1;
+/**
+ * 用来演示github的基本用法
+ * @author 窖头
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("王者农药伤不起啊！");
+	}
+
+}
